5f232cfed3743e7a4bde3b4aed5e11d28adcc294
Shawn Hooper <shawn@fivesense.ca>
2022-08-25 08:38:50 -0400

    Add foreign key id to the relationship

--- app/Models/ProductService/Product.php
+++ app/Models/ProductService/Product.php
@@ -4,14 +4,15 @@ namespace App\Models\ProductService;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     use HasFactory;
     protected $guarded = [];
-    public function Category()
+
+    public function Category(): BelongsTo
     {
-        
-        return $this->belongsTo(ProductCategory::class);
+        return $this->belongsTo(ProductCategory::class, 'product_category_id');
     }
 }
