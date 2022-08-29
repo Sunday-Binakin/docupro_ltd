35819e3f0c0417a8e83a051bc500f6d55a7ded12
Sunday-Binakin <sundaybinakin@gmail.com>
2022-08-29 08:28:07 +0100

    testing category

--- app/Models/ProductService/Product.php
+++ app/Models/ProductService/Product.php
@@ -11,7 +11,8 @@ class Product extends Model
     protected $guarded = [];
     public function Category()
     {
-        
+
         return $this->belongsTo(ProductCategory::class);
+       
     }
 }
